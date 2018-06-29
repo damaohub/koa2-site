8cd28fbb8b862a1d439a8ff16dc2c83e6784340c
damaohub <damao100@126.com>
2018-06-29 14:02:23 +0800

    分离路由和控制器

--- controllers/posts.js
+++ controllers/posts.js
@@ -10,6 +10,27 @@ const postService = require('../services/posts')
         })
     }
 
+    const PostById = async (ctx, next) => {
+        let data = await postService.getPost({ postID: ctx.params.id })
+        let _tags = await postService.getTagsBypost(ctx.params.id)
+        ctx.render('post.html', {
+            post: data,
+            tags: _tags
+        })
+    }
+
+    const PostsByTag = async (ctx, next) => {
+        let data = await postService.getPostsByTag(ctx.params.tag)
+        let _link = await postService.getLink(ctx, 'post/')
+        ctx.render('tag.html', {
+            posts: data,
+            tag: ctx.params.tag,
+            link: _link
+        })
+    }
+
 module.exports = {
-    index
+    index,
+    PostById,
+    PostsByTag
 }    
